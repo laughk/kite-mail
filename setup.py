7f12b55ead0bc0efccf5a6c6c7af9c7ae37b3417
Kei Iwasaki <me@laughk.org>
2015-07-06 00:23:31 +0900

    add desctiption

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='kite-mail',
-    description='',
+    description='kite-mail is a command-line tool to request the contents of e-mail to takosan .',
     version='0.0.0.dev0',
     packages=find_packages(),
     include_package_data=True,
