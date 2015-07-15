a0ba2bc288fd57dc6c10de0bd3fc6faab379556d
Kei Iwasaki <me@laughk.org>
2015-07-15 15:54:02 +0900

    add help messages, and add version infomation.

--- kite_mail/utils.py
+++ kite_mail/utils.py
@@ -11,10 +11,23 @@ def help_messages(command):
         'icon':
             'set image url or emoji (e.g. :shachikun:)',
         'body':
-            'the flag decide include mail body part. (Default: False) '
+            'the flag decide include mail body part. (Default: False) ',
+        'body_prefix':
+            'add prefix text message for mail body part. (Default: ":memo:" )',
+        'subject_prefix':
+            'add prefix text message for mail subject. (Default: ":round_pushpin:" )',
+        'from':
+            'the flag decide include mail from part. (Default: False) ',
+        'from_prefix':
+            'add prefix text message for mail from part. (Default: ":black_nib:" )',
+        'color':
+            'set color border along the left side of the message attachment [good|warning|danger|<hex color code>]  (e.g. #439FE0)',
     }
 
     if msg[command] :
         return msg[command]
     else:
         return None
+
+def version():
+    return '0.0.1.dev0'
